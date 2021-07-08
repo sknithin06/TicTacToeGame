3986c000abb19f8f46b8b7658bf8e423e7d54674
sknithin06 <nithinsk939@gmail.com>
2021-07-08 09:57:48 +0530

    UC1 is added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,17 @@
 package workshop;
+import java.util.Scanner;
 public class TicTacToeGame {
+	 static void printBoard(char []board) {
+		for ( int r=0; r<10; r++) {
+			board[r] = ' ';
+			System.out.println(board);
+		}
+	 }
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");
+		
+		 Scanner sc = new Scanner(System.in);
+		 char [] board = new char[10];
+
 	}
 }
